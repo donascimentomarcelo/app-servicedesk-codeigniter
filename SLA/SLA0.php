a651725164f2d1c9932d3b663ecd254a6d4a1187
OP0321-00012 <OP0321-00012@152.92.5.122>
2016-09-16 16:49:47 -0300

    Add image upload in insert user form (15/09/16)

--- SLA/SLA0.php
+++ SLA/SLA0.php
@@ -10,17 +10,6 @@
 </head>
 <body>
 <?php
-
-/* 
- * 1º - Criar cálculo onde eu pego o valor inicial(hora) da abertura do chamado, seto o valor final(hora) e faço o calculo onde 
- * verifico quanto do valor inicial falta para chegar no final, ou vice versa.
- * 2º - Cria o progress bar http://opensource.locaweb.com.br/locawebstyle/documentacao/componentes/barra-de-progresso/
- * 3º - Busco o valor da 1ª função e jogo dentro do progress bar dentro do style="width:<?php $valor ?>%" 
- * 4º - Coloco a div para atualizar por segundo
- * 5º - Para mudar a cor, devo salvar os nomes das class no banco e varificar, se tempo for igual a X atualizo o valor da class
- * <div class="progress-bar progress-bar-<?php $valor_da_class?>">
-*/
-
 $dataInicial = '16/09/2016';
 $sla = 1;
 
@@ -37,11 +26,26 @@ $porcentagem = ($minutoAtual * 100)/$sla;
 echo $horaAtual,':';
 echo $minutoAtual,'</br>';
 echo $porcentagem = (int)$porcentagem;
+
+if($porcentagem <= 25){
+    
 $class = 'success';
+
+}else if($porcentagem >=26 && $porcentagem <=80){
+    
+$class = 'warning';
+        
+}else{
+    
+$class = 'warning';
+    
+    
+}
 /*
  * 3 x 100 = 300 
  * 300 : 60 = 5% 
  */
+
 ?>
 
 
